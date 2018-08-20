2fc1d950c778fb142372df8405b6ff602256433f
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-08-20 23:34:10 +0200

    interval does not need to be stored explicitely

--- controller.py
+++ controller.py
@@ -7,12 +7,11 @@ class SimulatedLinearController(LinearController):
         super(SimulatedLinearController, self).__init__(target, low_utilisation, high_allocation, rate, interval)
         self.env = env
         self.action = env.process(self.run())
-        self._interval = interval
 
     def run(self):
         while True:
-            self.regulate(interval=self._interval)
+            self.regulate(interval=self.interval)
             # print("[controller] demand %d -> %d, supply %d (global %d), allocation %.2f, utilisation %.2f "
             #       "(available %d)" % (pre_demand, self.target.demand, self.target.supply, globals.global_demand.level,
             #                           self.target.allocation, self.target.utilisation, self.target.level))
-            yield self.env.timeout(self._interval)
+            yield self.env.timeout(self.interval)
