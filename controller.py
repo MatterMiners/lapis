afa7b26de1be65d147e081988a2e81d2e44d63de
matthias.schnepf <matthias.schnepf@kit.edu>
2018-08-20 16:28:01 +0200

    update controller due to cobald commit https://github.com/MaineKuehn/cobald/commit/7108f7bff9fa5d6c9e5d566f0e44f88a2197cb43#diff-ea5ffbd14c2ae4836b4ca0400af9ecee

--- controller.py
+++ controller.py
@@ -3,14 +3,15 @@ from cobald.interfaces import Pool
 
 
 class SimulatedLinearController(LinearController):
-    def __init__(self, env, target: Pool, low_utilisation=0.5, high_allocation=0.5, rate=1):
-        super(SimulatedLinearController, self).__init__(target, low_utilisation, high_allocation, rate)
+    def __init__(self, env, target: Pool, low_utilisation=0.5, high_allocation=0.5, rate=1, interval=1):
+        super(SimulatedLinearController, self).__init__(target, low_utilisation, high_allocation, rate, interval)
         self.env = env
         self.action = env.process(self.run())
+        self._interval = interval
 
     def run(self):
         while True:
-            self.regulate_demand()
+            self.regulate(interval=self._interval)
             # print("[controller] demand %d -> %d, supply %d (global %d), allocation %.2f, utilisation %.2f "
             #       "(available %d)" % (pre_demand, self.target.demand, self.target.supply, globals.global_demand.level,
             #                           self.target.allocation, self.target.utilisation, self.target.level))
