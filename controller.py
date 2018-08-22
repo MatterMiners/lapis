31fa3aef61aeeea3e0f0056475cef5a01c02b642
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-08-22 18:49:46 +0200

    added an experimental controller

--- controller.py
+++ controller.py
@@ -15,3 +15,24 @@ class SimulatedLinearController(LinearController):
             #       "(available %d)" % (pre_demand, self.target.demand, self.target.supply, globals.global_demand.level,
             #                           self.target.allocation, self.target.utilisation, self.target.level))
             yield self.env.timeout(self.interval)
+
+
+class SimulatedCostController(SimulatedLinearController):
+    def __init__(self, env, target: Pool, low_utilisation=0.5, high_allocation=0.5, rate=1, interval=1):
+        self.current_cost = 1
+        super(SimulatedCostController, self).__init__(env, target, low_utilisation, high_allocation, rate, interval)
+
+    def regulate(self, interval):
+        allocation = 0
+        for drone in self.target.drones:
+            allocation += drone.allocation
+        if self.target.supply - allocation <= 1:
+            if self.target.utilisation >= .8:
+                self.target.demand = int(allocation + self.current_cost)
+                self.current_cost += 1
+            else:
+                self.target.demand = allocation
+                if self.current_cost > 1:
+                    self.current_cost -= 1
+        # else:
+        #     self.target.demand = allocation
