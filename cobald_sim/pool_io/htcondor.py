40252808180c4dafd53c51acd4c1d4cb428bd741
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-10-23 22:23:20 +0200

    improved reading of pools from htcondor

--- cobald_sim/pool_io/htcondor.py
+++ cobald_sim/pool_io/htcondor.py
@@ -4,9 +4,9 @@ from ..pool import StaticPool
 
 
 def htcondor_pool_reader(env, iterable, resource_name_mapping={
-    "TotalSlotCPUs": "cores",
-    "TotalSlotDisk": "disk",
-    "TotalSlotMemory": "memory"
+    "cores": "TotalSlotCPUs",
+    "disk": "TotalSlotDisk",
+    "memory": "TotalSlotMemory"
 }):
     """
     Load a pool configuration that was exported via htcondor from files or iterables
@@ -15,10 +15,9 @@ def htcondor_pool_reader(env, iterable, resource_name_mapping={
     :param resource_name_mapping: Mapping from given header names to well-defined resources in simulation
     :return: Yields the :py:class:`StaticPool`s found in the given iterable
     """
-    reader = csv.reader(iterable, delimiter=' ', skipinitialspace=True)
-    first_line = next(reader)
+    reader = csv.DictReader(iterable, delimiter=' ', skipinitialspace=True)
     for row_idx, row in enumerate(reader):
         yield StaticPool(
             env,
-            init=int(row[0]),
-            resources={value: row[first_line.index(key)] for key, value in resource_name_mapping.items()})
+            init=int(row["Count"]),
+            resources={key: row[value] for key, value in resource_name_mapping.items()})
