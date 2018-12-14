a31544cbaad49e57b74595e96619d8f1f5a411fd
Eileen Kuehn <eileen.kuehn@kit.edu>
2018-12-14 15:19:03 +0100

    added test for import of htcondor

---
+++ lapis_tests/job_io/test_htcondor.py
@@ -0,0 +1,11 @@
+from lapis.job_io.htcondor import htcondor_job_reader
+
+
+class TestHtcondorJobReader(object):
+    def test_simple_read(self):
+        with open("../data/condor_usage_sorted_filtered-ek.csv") as input_file:
+            jobs = 0
+            for job in htcondor_job_reader(None, input_file):
+                assert job is not None
+                jobs += 1
+            assert jobs > 0
