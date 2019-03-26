56f33b8c47f67f3aa45ac730b30760be84b3ea8f
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-03-26 16:42:19 +0100

    module index for documentation

--- lapis/__about__.py
+++ lapis/__about__.py
@@ -12,5 +12,5 @@ __url__ = 'https://github.com/MaineKuehn/lapis'
 __version__ = '0.1.0'
 __author__ = 'Eileen Kuehn, Max Fischer'
 __email__ = 'mainekuehn@gmail.com'
-__copyright__ = '2018 %s' % __author__
+__copyright__ = '2019 %s' % __author__
 __keywords__ = 'opportunistic scheduling scheduler cobald simulator'
