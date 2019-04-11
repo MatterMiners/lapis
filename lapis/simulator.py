80b33cff9f3e6ff94d2fa11d9c6629a8b87bcd1e
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-04-11 16:06:14 +0200

    fixes to simulator to also support simulation until end

--- lapis/simulator.py
+++ lapis/simulator.py
@@ -40,19 +40,18 @@ class Simulator(object):
         for pool in pool_reader(iterable=pool_input, pool_type=pool_type, make_drone=partial(Drone, self.job_scheduler)):
             self.pools.append(pool)
             if controller:
-                controller(target=pool, rate=1)
-                self.controllers.append(controller)
+                self.controllers.append(controller(target=pool, rate=1))
 
     def create_scheduler(self, scheduler_type):
         self.job_scheduler = scheduler_type(job_queue=self.job_queue)
 
-    def run(self, until=2000):
+    def run(self, until=None):
         print("running until", until)
         run(self._simulate(until))
 
     async def _simulate(self, end):
         print("Starting simulation at %s" % time.now)
-        async with until(time == end) as while_running:
+        async with until(time == end) if end else Scope() as while_running:
             for pool in self.pools:
                 while_running.do(pool.run())
             for job_input, job_reader in self._job_generators:
