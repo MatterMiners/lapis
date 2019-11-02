4e0e4ff8c7d2d413f6f2eca5cf660137486bbf36
tfesenbecker <tabea.fessenbecker@student.kit.edu>
2019-11-02 10:39:54 +0100

    unittest with strange JSONDecoderError

--- lapis_tests/job_io/test_htcondor.py
+++ lapis_tests/job_io/test_htcondor.py
@@ -19,3 +19,22 @@ class TestHtcondorJobReader(object):
             # ensure that one job was removed by importer (wrong walltime given)
             lines = sum(1 for _ in input_file)
             assert jobs == (lines - 2)
+
+    def test_read_with_inputfiles(self):
+        with open(
+            os.path.join(
+                os.path.dirname(__file__), "..", "data", "job_list_minimal.json"
+            )
+        ) as input_file:
+            print(
+                os.path.join(
+                    os.path.dirname(__file__), "..", "data", "job_list_minimal.json"
+                )
+            )
+            jobs = 0
+            # lines = sum(1 for _ in input_file)
+            for job in htcondor_job_reader(input_file):
+                assert job is not None
+                jobs += 1
+            assert jobs > 0
+            # assert jobs == (lines - 1)
