110b3e9354529ca86b17c7cc92c9f885bc43fe97
tfesenbecker <tabea.fessenbecker@student.kit.edu>
2019-11-02 17:07:31 +0100

    replaced function modifying walltime by function with property decorator

--- lapis/utilities/walltime_models.py
+++ lapis/utilities/walltime_models.py
@@ -1,5 +1,7 @@
-def extrapolate_walltime_to_maximal_efficiency(job, maximal_efficiency: float = 0.8):
-    return (job.used_resources["cores"] / maximal_efficiency) * job.walltime
+def extrapolate_walltime_to_maximal_efficiency(
+    job, original_walltime, maximal_efficiency: float = 0.8
+):
+    return (job.used_resources["cores"] / maximal_efficiency) * original_walltime
 
 
 # TODO: add models depending on fraction of cached files etc
