a3fdaa2d0341c1b583a88ee04eb0dd373f92678e
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-04 16:22:10 +0100

    fixed config for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-import lapis
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import lapis  # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
