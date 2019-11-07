2d979a97bdb80222d462afb96ac3eba55d805172
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-07 22:41:22 +0100

    added dependency to sphinx-click

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ extensions = [
     "sphinx.ext.imgmath",
     "sphinx.ext.viewcode",
     "sphinxcontrib.contentui",
+    "sphinx_click.ext",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
