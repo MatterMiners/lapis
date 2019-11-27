6df1b27e2af08e5c09d6e0cb88f38684f4b01b5e
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-27 20:14:29 +0100

    converted storage and file sizes to bytes

--- lapis/storage_io/storage.py
+++ lapis/storage_io/storage.py
@@ -2,20 +2,35 @@ import csv
 from lapis.storage import Storage
 
 
-def storage_reader(storage, storage_content):
+def storage_reader(
+    storage,
+    storage_content,
+    unit_conversion_mapping: dict = {
+        "cachesizeGB": 1024 * 1024 * 1024,
+        "throughput_limit": 1024 * 1024 * 1024,
+    },  # noqa: B006
+):
     storage_content = storage_content_reader(storage_content)
     reader = csv.DictReader(storage, delimiter=" ", quotechar="'")
     for row in reader:
         yield Storage(
             name=row["name"],
             sitename=row["sitename"],
-            storagesize=int(row["cachesizeGB"]),
+            storagesize=int(
+                row["cachesizeGB"] * unit_conversion_mapping.get("cachesizeGB", 1)
+            ),
             throughput_limit=int(row["throughput_limit"]),
             files=storage_content[row["name"]],
         )
 
 
-def storage_content_reader(file_name):
+def storage_content_reader(
+    file_name,
+    unit_conversion_mapping: dict = {
+        "filesize": 1024 * 1024 * 1024,
+        "usedsize": 1024 * 1024 * 1024,
+    },
+):
     reader = csv.DictReader(file_name, delimiter=" ", quotechar="'")
     cache_information = dict()
     for row in reader:
@@ -29,7 +44,9 @@ def storage_content_reader(file_name):
             "lastaccessed",
             "numberofaccesses",
         ]:
-            cache_information[row["cachename"]][row["filename"]][key] = int(row[key])
+            cache_information[row["cachename"]][row["filename"]][key] = int(
+                row[key] * unit_conversion_mapping.get(key, 1)
+            )
     if not cache_information:
         cache_information = None
     return cache_information
