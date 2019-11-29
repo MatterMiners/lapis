0019583638232203deb364191247554a5e65ddb3
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-29 12:30:50 +0100

    extended tests

--- lapis_tests/storage_io/test_storage.py
+++ lapis_tests/storage_io/test_storage.py
@@ -5,20 +5,21 @@ from lapis.storage_io.storage import storage_reader
 
 
 class TestStorageReader(object):
-    def _create_simple_config(self):
+    def _create_simple_config(self, to_string=False):
         storage_config = NamedTemporaryFile(suffix=".csv")
         with open(storage_config.name, "w") as write_stream:
             write_stream.write(
-                "name sitename cachesizeGB throughput_limit\n" "name sitename 10.1 1"
+                f"name sitename cachesizeGB throughput_limit\n"
+                f"name sitename {str(10) if to_string else 10} {str(10.1) if to_string else 10.1} {str(1) if to_string else 1}"
             )
         return storage_config
 
-    def _create_simple_files(self):
+    def _create_simple_files(self, to_string=False):
         file_config = NamedTemporaryFile(suffix=".csv")
         with open(file_config.name, "w") as write_stream:
             write_stream.write(
-                "filename cachename filesize storedsize cachedsince lastaccessed numberofaccesses\n"
-                "file name 10.1 5.0 0 0 1"
+                f"filename cachename filesize storedsize cachedsince lastaccessed numberofaccesses\n"
+                f"file name {str(10.1) if to_string else 10.1} {str(5.0) if to_string else 5.0} {str(0) if to_string else 0} {str(0) if to_string else 0} {str(1) if to_string else 1}"
             )
         return file_config
 
@@ -33,16 +34,20 @@ class TestStorageReader(object):
         assert count == 1
 
     def test_simple_read(self):
-        simple_config = self._create_simple_config()
-        simple_files = self._create_simple_files()
-        count = 0
-        for storage in storage_reader(
-            open(simple_config.name, "r"), open(simple_files.name, "r"), StorageElement
-        ):
-            assert storage is not None
-            assert type(storage.available) == int
-            assert storage.available == int(5.1 * 1024 * 1024 * 1024)
-            assert type(storage.size) == int
-            assert storage.size == int(10.1 * 1024 * 1024 * 1024)
-            count += 1
-        assert count == 1
+        for variant in [False, True]:
+            print(f"starting with {variant}")
+            simple_config = self._create_simple_config(to_string=variant)
+            simple_files = self._create_simple_files(to_string=variant)
+            count = 0
+            for storage in storage_reader(
+                open(simple_config.name, "r"),
+                open(simple_files.name, "r"),
+                StorageElement,
+            ):
+                assert storage is not None
+                assert type(storage.available) == int
+                assert storage.available == int(5.1 * 1024 * 1024 * 1024)
+                assert type(storage.size) == int
+                assert storage.size == int(10.1 * 1024 * 1024 * 1024)
+                count += 1
+            assert count == 1
