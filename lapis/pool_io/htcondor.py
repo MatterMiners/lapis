453257e3cf8c9914e51db3f71fa72c2fcf8621de
Eileen Kuehn <eileen.kuehn@kit.edu>
2020-12-11 22:13:19 +0100

    cleaned api of pool importers

--- lapis/pool_io/htcondor.py
+++ lapis/pool_io/htcondor.py
@@ -1,29 +1,34 @@
 import csv
 from functools import partial
 
-from typing import Callable
+from typing import Callable, Dict, Iterable
 from ..pool import Pool
+
+
+default_resource_name_mapping: Dict[str, str] = {
+    "cores": "TotalSlotCPUs",
+    "disk": "TotalSlotDisk",  # MiB
+    "memory": "TotalSlotMemory",  # MiB
+}
+default_unit_conversion_mapping: Dict[str, float] = {
+    "TotalSlotCPUs": 1,
+    "TotalSlotDisk": 1024 * 1024,
+    "TotalSlotMemory": 1024 * 1024,
+}
 
 
 def htcondor_pool_reader(
     iterable,
-    resource_name_mapping: dict = {  # noqa: B006
-        "cores": "TotalSlotCPUs",
-        "disk": "TotalSlotDisk",  # MiB
-        "memory": "TotalSlotMemory",  # MiB
-    },
-    unit_conversion_mapping: dict = {  # noqa: B006
-        "TotalSlotCPUs": 1,
-        "TotalSlotDisk": 1024 * 1024,
-        "TotalSlotMemory": 1024 * 1024,
-    },
+    resource_name_mapping: Dict[str, str] = None,
+    unit_conversion_mapping: Dict[str, float] = None,
     pool_type: Callable = Pool,
     make_drone: Callable = None,
-):
+) -> Iterable[Pool]:
     """
     Load a pool configuration that was exported via htcondor from files or
     iterables
 
+    :param unit_conversion_mapping:
     :param iterable: an iterable yielding lines of CSV, such as an open file
     :param resource_name_mapping: Mapping from given header names to well-defined
                                   resources in simulation
@@ -31,6 +36,11 @@ def htcondor_pool_reader(
     :param make_drone:
     :return: Yields the :py:class:`Pool`s found in the given iterable
     """
+    if resource_name_mapping is None:
+        resource_name_mapping = default_resource_name_mapping
+    if unit_conversion_mapping is None:
+        unit_conversion_mapping = default_unit_conversion_mapping
+
     assert make_drone
     reader = csv.DictReader(iterable, delimiter=" ", skipinitialspace=True)
     for row in reader:
--- lapis/pool_io/machines.py
+++ lapis/pool_io/machines.py
@@ -1,23 +1,27 @@
 import csv
 from functools import partial
 
-from typing import Callable
+from typing import Callable, Dict, Iterable
 from ..pool import Pool
+
+
+default_resource_name_mapping: Dict[str, str] = {
+    "cores": "CPUs_per_node",
+    "memory": "RAM_per_node_in_KB",
+}
+default_unit_conversion_mapping: Dict[str, float] = {
+    "CPUs_per_node": 1,
+    "RAM_per_node_in_KB": 1000,
+}
 
 
 def machines_pool_reader(
     iterable,
-    resource_name_mapping: dict = {  # noqa: B006
-        "cores": "CPUs_per_node",
-        "memory": "RAM_per_node_in_KB",
-    },
-    unit_conversion_mapping={  # noqa: B006
-        "CPUs_per_node": 1,
-        "RAM_per_node_in_KB": 1000,
-    },
+    resource_name_mapping: Dict[str, str] = None,
+    unit_conversion_mapping: Dict[str, float] = None,
     pool_type: Callable = Pool,
     make_drone: Callable = None,
-):
+) -> Iterable[Pool]:
     """
     Load a pool configuration that was exported via htcondor from files or
     iterables
@@ -29,6 +33,11 @@ def machines_pool_reader(
     :param pool_type: The type of pool to be yielded
     :return: Yields the :py:class:`StaticPool`s found in the given iterable
     """
+    if resource_name_mapping is None:
+        resource_name_mapping = default_resource_name_mapping
+    if unit_conversion_mapping is None:
+        unit_conversion_mapping = default_unit_conversion_mapping
+
     assert make_drone
     reader = csv.DictReader(iterable, delimiter=" ", skipinitialspace=True)
     for row in reader:
