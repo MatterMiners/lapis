f3acf6721f1f18b4c408c452a1b24805e5cc7dcb
Eileen Kuehn <eileen.kuehn@kit.edu>
2020-12-11 20:06:57 +0100

    cleaned api of importers

--- lapis/job_io/htcondor.py
+++ lapis/job_io/htcondor.py
@@ -1,36 +1,49 @@
 import csv
 import json
 import logging
+from typing import Dict, Iterable
 
 from lapis.job import Job
 from copy import deepcopy
 
 
+default_resource_name_mapping: Dict[str, str] = {
+    "cores": "RequestCpus",
+    "walltime": "RequestWalltime",  # s
+    "memory": "RequestMemory",  # MiB
+    "disk": "RequestDisk",  # KiB
+}
+default_used_resource_name_mapping: Dict[str, str] = {
+    "queuetime": "QDate",
+    "walltime": "RemoteWallClockTime",  # s
+    "memory": "MemoryUsage",  # MB
+    "disk": "DiskUsage_RAW",  # KiB
+}
+default_unit_conversion_mapping: Dict[str, float] = {
+    "RequestCpus": 1,
+    "RequestWalltime": 1,
+    "RequestMemory": 1024 * 1024,
+    "RequestDisk": 1024,
+    "queuetime": 1,
+    "RemoteWallClockTime": 1,
+    "MemoryUsage": 1000 * 1000,
+    "DiskUsage_RAW": 1024,
+}
+
+
 def htcondor_job_reader(
     iterable,
-    resource_name_mapping={  # noqa: B006
-        "cores": "RequestCpus",
-        "walltime": "RequestWalltime",  # s
-        "memory": "RequestMemory",  # MiB
-        "disk": "RequestDisk",  # KiB
-    },
-    used_resource_name_mapping={  # noqa: B006
-        "queuetime": "QDate",
-        "walltime": "RemoteWallClockTime",  # s
-        "memory": "MemoryUsage",  # MB
-        "disk": "DiskUsage_RAW",  # KiB
-    },
-    unit_conversion_mapping={  # noqa: B006
-        "RequestCpus": 1,
-        "RequestWalltime": 1,
-        "RequestMemory": 1024 * 1024,
-        "RequestDisk": 1024,
-        "queuetime": 1,
-        "RemoteWallClockTime": 1,
-        "MemoryUsage": 1000 * 1000,
-        "DiskUsage_RAW": 1024,
-    },
-):
+    resource_name_mapping: Dict[str, str] = None,
+    used_resource_name_mapping: Dict[str, str] = None,
+    unit_conversion_mapping: Dict[str, float] = None,
+) -> Iterable[Job]:
+    if resource_name_mapping is None:
+        resource_name_mapping = default_resource_name_mapping
+    if used_resource_name_mapping is None:
+        used_resource_name_mapping = default_used_resource_name_mapping
+    if unit_conversion_mapping is None:
+        unit_conversion_mapping = default_unit_conversion_mapping
+
     input_file_type = iterable.name.split(".")[-1].lower()
     if input_file_type == "json":
         htcondor_reader = json.load(iterable)
@@ -40,6 +53,7 @@ def htcondor_job_reader(
         logging.getLogger("implementation").error(
             "Invalid input file %s. Job input file can not be read." % iterable.name
         )
+        return
     for entry in htcondor_reader:
         if float(entry[used_resource_name_mapping["walltime"]]) <= 0:
             logging.getLogger("implementation").warning(
